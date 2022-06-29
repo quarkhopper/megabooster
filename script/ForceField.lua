03081d8f05cb9dea560d90e3b4ca8c2505f97f0a
Quarkhopper <quarkhopper@gmail.com>
2022-06-29 00:56:37 -0700

    stuff

--- script/ForceField.lua
+++ script/ForceField.lua
@@ -100,7 +100,8 @@ function propagate_point_force(ff, point, dt)
             resultant_dir = VecAdd(resultant_dir, VecScale(trans_dir, trans_mag))
         else
             local hit_point = VecAdd(point.pos, VecScale(trans_dir, dist))
-            resultant_dir = VecAdd(resultant_dir, VecScale(normal, point.mag))
+            trans_dir = VecNormalize(VecAdd(trans_dir, normal))
+            resultant_dir = VecAdd(resultant_dir, VecScale(trans_dir, point.mag))
             -- table.insert(ff.contacts, inst_field_contact(point, hit_point, normal, shape))
             -- point.mag = point.mag * (1 - ff.thermo_loss)        
             point.cull = true
